48e8163a4ea78684cdd056a78b3a98244cff7dfa
pau <paula.martilloq@gmail.com>
2026-05-07 17:30:21 -0500

    Primeros ejercicios

---
+++ taller_prolog.pl
@@ -0,0 +1,12 @@
+tiene(juan,bicicleta).
+tiene(juan,coche).
+
+% que cosas tiene en comun juan y ana
+tiene(ana,coche).
+
+tiene(pedro,moto).
+
+% formato: libro(titulo, autor, editorial, año).
+tiene('Juan Lora', libro('Ana Karenina', 'Tolstoi', 'Luna', 2010)).
+tiene('Juan Lora', libro('El ingenioso caballero Don Quijote de la Mancha', 'Miguel de Cervantes', 'RAE', 2015)).
+
---
+++ taller_prolog_2.pl
@@ -0,0 +1,9 @@
+cuadrado(X,R):- R is X*X.
+
+media(A,B,M):- M is (A+B)/2.
+
+factorial(0,1).
+factorial(N,R):-
+    N1 is N-1,
+    factorial(N1,R1),
+    R is N*R1.
